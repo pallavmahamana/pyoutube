d3d86c55760be1e144384354a3ac4bc7b315aa65
Pallav M <spsec.pallav@gmail.com>
2021-09-23 21:52:50 +0530

    added main.py youtube.py

---
+++ pyoutube/__init__.py
@@ -0,0 +1,8 @@
+" pyoutube -- Console Youtube Audio Player"
+
+version = (0, 0, 0, 1)
+
+__version__ = version = '.'.join(map(str, version_info))
+__project__ = __name__
+__author__ = "Pallav Mahamana"
+__license__ = "MIT"
